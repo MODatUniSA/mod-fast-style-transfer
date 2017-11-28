e58506d541cfc56b29b4ca9966963e2cd9f962f6
sighmon <simon.loffler@gmail.com>
2017-11-28 14:26:49 +1030

    Perhaps don't spell From with a capital.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # A simple web server to receive images, transfer style to them, and post them elsewhere.
 
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'MOD. says hi hello. x'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
