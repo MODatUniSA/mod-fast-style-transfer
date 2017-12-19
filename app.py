95337c7372f08b8dcead0b46d9a91312e8fc410b
sighmon <simon.loffler@gmail.com>
2017-12-19 11:09:55 +1030

    Returning both images in a JSON get request of the image number.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # A simple web server to receive images, transfer style to them, and post them elsewhere.
 
 from flask import Flask, request
-from flask_restful import Resource, Api, abort
+from flask_restful import Resource, Api, abort, reqparse
 import base64, subprocess, pdb
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ api = Api(app)
 
 class HelloMOD(Resource):
   def get(self):
-    return {'hello': 'MOD. says hi hello. x'}
+    return {'hello': 'MOD. image style transfer server says hi hello. x'}
 
 api.add_resource(HelloMOD, '/')
 
@@ -27,7 +27,20 @@ def abort_if_image_doesnt_exist(image_id):
 class PostImage(Resource):
   def get(self, image_id):
     abort_if_image_doesnt_exist(image_id)
-    return {image_id: images[image_id]}
+    # parser = reqparse.RequestParser()
+    # parser.add_argument('Content-Type', location='headers')
+    # args = parser.parse_args()
+    # request_type = args['Content-Type']
+    # pdb.set_trace()
+    image_file_out_path = "out/image{}.png".format(image_id)
+    encoded_deep_dream_image_string = ""
+    try:
+      with open(image_file_out_path, "rb") as image_file:
+        encoded_deep_dream_image_string = base64.b64encode(image_file.read()).decode('utf-8')
+    except Exception as e:
+      print("PROBLEM! {}".format(e))
+    
+    return {image_id: images[image_id], "{}_output".format(image_id): encoded_deep_dream_image_string}
 
   def put(self, image_id):
     # Decode Base64 image
@@ -48,7 +61,7 @@ class PostImage(Resource):
     # If ever we need the MIME type
     # image_mime_type = subprocess.check_output(['file', '--mime', '-b', image_file_path]).decode('utf-8')
 
-    # TODO: run it through deep dream
+    # Transfer the image style
     print("Starting to dream...")
     try:
       deep_dream_output = subprocess.check_output(['python', 'evaluate.py', '--checkpoint', 'udlf_fst_checkpoints/scream.ckpt', '--in-path', image_file_path, '--out-path', image_file_out_path]).decode('utf-8')
@@ -58,11 +71,11 @@ class PostImage(Resource):
     print(deep_dream_output)
     print("I've finished dreaming...")
 
-    # TODO: load back the dreamt image
+    # Load the styalised image
     with open(image_file_out_path, "rb") as image_file:
       encoded_deep_dream_image_string = base64.b64encode(image_file.read()).decode('utf-8')
 
-    # TODO: return Base64 image back
+    # Return Base64 image back
     return {image_id: encoded_deep_dream_image_string}
 
 api.add_resource(PostImage, '/<string:image_id>')
